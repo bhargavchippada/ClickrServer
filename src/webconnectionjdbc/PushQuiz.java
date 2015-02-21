7bd1e17a6acb85192bfab66c917c4cc0be0de838
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-02-22 00:07:40 +0530

    PushQuiz exchanges json data

--- src/webconnectionjdbc/PushQuiz.java
+++ src/webconnectionjdbc/PushQuiz.java
@@ -1,103 +1,89 @@
 package webconnectionjdbc;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.PrintWriter;
 import java.util.HashMap;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import support.ByteBuffer;
 import support.MIMETypeConstantsIF;
 import support.Utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import datahandler.Question;
 
 public class PushQuiz extends HttpServlet{
-	
+	private static final long serialVersionUID = 2117385637713944115L;
 	String classname = "PushQuiz";
-	
-	@Override 
-	protected void doGet(HttpServletRequest req, HttpServletResponse res)
-		    throws ServletException, IOException
-	{
-	  ServletOutputStream sos = res.getOutputStream();
+	HttpSession mySession;
 
-	  res.setContentType(MIMETypeConstantsIF.PLAIN_TEXT_TYPE);
-	  sos.write("This is the PushQuiz servlet".getBytes());
-	  Utils.println("Authentication doGet Method was called...");
-	  sos.flush();
-	  sos.close();
-
-	}
-	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		ByteBuffer inputBB = new ByteBuffer(request.getInputStream());
-		ByteBuffer outputBB = null;
-		  try {
-	
-		    // extract the hashmap
-			Utils.logv(classname, "trying to extract hashtable from request");
-	
-		    ObjectInputStream ois = new ObjectInputStream(inputBB.getInputStream());
-		    HashMap<String, String> input = (HashMap<String, String>) ois.readObject();
-		    
-		    Utils.logv(classname, "got the uid/pwd from the client:" + input);
-	
-		    Object retval = _processInput(input);
-		    Utils.logv(classname, "created response hashtable, sending it back");
-	
-		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		    ObjectOutputStream oos = new ObjectOutputStream(baos);
-		    oos.writeObject(retval);
-		    
-		    outputBB = new ByteBuffer(baos.toByteArray());		    
-	
-		    Utils.println("sent response back...");
-	
-		  }
-		  catch (Exception e) {
-			Utils.println("Error processing response");
-		    System.out.println(e);
-		    e.printStackTrace();
-		  }
-	
-		  ServletOutputStream sos = response.getOutputStream();
-	
-		  if (outputBB != null) {
-		    response.setContentType(MIMETypeConstantsIF.BINARY_TYPE);
-		    response.setContentLength(outputBB.getSize());
-		    sos.write(outputBB.getBytes());
-		  }
-		  else {
-			HashMap<String, String> output = new HashMap<String, String>();
-			output.put("status","0");
-			outputBB = Utils.convertToBB(output);
-		    response.setContentType(MIMETypeConstantsIF.BINARY_TYPE);
-		    response.setContentLength(outputBB.getSize());
-		    sos.write(outputBB.getBytes());
-		  }
-	
-		  sos.flush();
-		  sos.close();
+		Utils.logv(classname, "Protocol: "+request.getProtocol());
+		Utils.logv(classname, "Ip: "+request.getRemoteHost());
+		Utils.logv(classname, "port: "+request.getRemotePort());
+
+		mySession = request.getSession(true);
+		Cookie[] cookies = request.getCookies();
+		HashMap<String, Object> cookies_map = new HashMap<String, Object>();
+		if(cookies!=null){
+			for(int i=0; i<cookies.length; i++){
+				cookies_map.put(cookies[i].getName(), cookies[i].getValue());
+			}
+			Utils.logv(classname, "cookie: "+cookies_map);
+		}
+
+		JsonObject output = null;
+		try {
+			Utils.logv(classname, "trying to extract json object from request");
+			JsonElement jelement = new JsonParser().parse(request.getReader());
+			JsonObject  input = jelement.getAsJsonObject();
+			Utils.logv(classname, "got the request from client:" + input.toString());
+
+			output = _processInput(input, request, response);
+			Utils.logv(classname, "created response, sending it back...");
+		}catch (Exception e) {
+			Utils.logv(classname,"Error processing response");
+			e.printStackTrace();
+		}
+
+		PrintWriter out = response.getWriter();
+
+		if (output != null) {
+			response.setContentType(MIMETypeConstantsIF.JSON_TYPE);
+			response.setContentLength(output.toString().getBytes().length);
+			out.print(output);
+			Utils.logv(classname, "response: "+output.toString());
+		}else {
+			output = new JsonObject();
+			output.addProperty("status",3); // error processing request
+			response.setContentType(MIMETypeConstantsIF.JSON_TYPE);
+			response.setContentLength(output.toString().getBytes().length);
+			out.print(output);
+			Utils.logv(classname, "response: "+output.toString());
+		}
+
+		out.flush();
+		out.close();
 	}
-	
-	/** actually get user profile object from service and send it back */
-	private HashMap<String, Serializable> _processInput(HashMap<String, String> input) {
-		
-		HashMap<String, Serializable> output = new HashMap<String, Serializable>();
-		output.put("questionContent",Question.questionContent);
-		output.put("quesType",Question.quesType+"");
-		output.put("options",Question.options);
-		output.put("status","1");
+
+	private JsonObject _processInput(JsonObject input, HttpServletRequest request, HttpServletResponse response) {
+		JsonObject output = new JsonObject();
+		output.addProperty("status",1);
+		output.addProperty("questionContent",Question.questionContent);
+		output.addProperty("quesType",Question.quesType);
+		JsonElement joptions = (new Gson()).toJsonTree(Question.options);
+		output.add("options",joptions.getAsJsonArray());
 		return output;
 	}
 }
