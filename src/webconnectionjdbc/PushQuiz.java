642876cfd5e59620c50d640872faad81e0b95fa9
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-03-31 04:49:07 +0530

    pushquiz servlet implemented

--- src/webconnectionjdbc/PushQuiz.java
+++ src/webconnectionjdbc/PushQuiz.java
@@ -2,10 +2,8 @@ package webconnectionjdbc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,12 +11,13 @@ import javax.servlet.http.HttpSession;
 
 import support.Utils;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import datahandler.ClassRoom;
 import datahandler.Question;
+import datahandler.UserProfile;
 
 public class PushQuiz extends HttpServlet{
 	private static final long serialVersionUID = 3622456660944045304L;
@@ -28,19 +27,8 @@ public class PushQuiz extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Utils.logv(classname, "Protocol: "+request.getProtocol());
-		Utils.logv(classname, "Ip: "+request.getRemoteHost());
-		Utils.logv(classname, "port: "+request.getRemotePort());
-
-		mySession = request.getSession(true);
-		Cookie[] cookies = request.getCookies();
-		HashMap<String, Object> cookies_map = new HashMap<String, Object>();
-		if(cookies!=null){
-			for(int i=0; i<cookies.length; i++){
-				cookies_map.put(cookies[i].getName(), cookies[i].getValue());
-			}
-			Utils.logv(classname, "cookie: "+cookies_map);
-		}
+		Utils.logv(classname, "servlet task - start");
+		long startTime = System.currentTimeMillis();
 
 		JsonObject output = null;
 		try {
@@ -65,7 +53,7 @@ public class PushQuiz extends HttpServlet{
 			Utils.logv(classname, "response: "+output.toString());
 		}else {
 			output = new JsonObject();
-			output.addProperty("status",3); // error processing request
+			output.addProperty("status",-1); // error processing request
 			response.setContentType(Utils.JSON_TYPE);
 			response.setContentLength(output.toString().getBytes().length);
 			out.print(output);
@@ -74,15 +62,45 @@ public class PushQuiz extends HttpServlet{
 
 		out.flush();
 		out.close();
+
+		Utils.logv(classname,"servlet task - end");
+		long stopTime = System.currentTimeMillis();
+		long elapsedTime = stopTime - startTime;
+		Utils.logv(classname,elapsedTime+"ms");
 	}
 
 	private JsonObject _processInput(JsonObject input, HttpServletRequest request, HttpServletResponse response) {
+		HttpSession mySession = request.getSession(false);
+
+		String uid = input.get("uid").getAsString();
+
 		JsonObject output = new JsonObject();
-		output.addProperty("status",1);
-		output.addProperty("questionContent",Question.question);
-		output.addProperty("quesType",Question.type);
-		JsonElement joptions = (new Gson()).toJsonTree(Question.options);
-		output.add("options",joptions.getAsJsonArray());
+		if(mySession==null || !(ClassRoom.serveronline)){
+			output.addProperty("status",0); //not authorized
+		}else if(!Question.startquiz || !Question.savedquiz){
+			output.addProperty("status",1); //quiz hasn't started yet
+		}else{
+			Object username = mySession.getAttribute("username");
+			Object password = mySession.getAttribute("password");
+
+			if(username!=null && password!=null){
+				UserProfile user = ClassRoom.users_map.get((String)username);
+				if(user!=null && user.password.equals((String) password) && uid.equals((String) username)){
+					output.addProperty("status",2); //user quiz can start
+					user.status = 2;
+					output.addProperty("title", Question.title);
+					output.addProperty("question", Question.question);
+					output.addProperty("type", Question.type);
+					output.add("options", Question.options);
+					output.addProperty("feedback", Question.feedback);
+					output.addProperty("timed", Question.timed);
+					output.addProperty("time", Question.time);
+				}else{
+					output.addProperty("status",0); //not authorized
+				}
+			}
+		}
+
 		return output;
 	}
 }
