2b1dac584121ae9d93421ce9fd58f88d66052387
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-03-31 01:43:11 +0530

    removed cookies, AuthenticationWS is the general template

--- src/webconnectionjdbc/Authentication.java
+++ src/webconnectionjdbc/Authentication.java
@@ -2,10 +2,8 @@ package webconnectionjdbc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,29 +18,16 @@ import com.google.gson.JsonParser;
 import datahandler.ClassRoom;
 import datahandler.UserProfile;
 
-// Tells whether the 
 public class Authentication extends HttpServlet{
-	private static final long serialVersionUID = -340324876755957651L;
-	String classname = "Authentication";
-	HttpSession mySession;
-	
+	private static final long serialVersionUID = 8842827877500138557L;
+	private static String classname = "Authentication";
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Utils.logv(classname, "Protocol: "+request.getProtocol());
-		Utils.logv(classname, "Ip: "+request.getRemoteHost());
-		Utils.logv(classname, "port: "+request.getRemotePort());
-		
-		mySession = request.getSession(true);
-		Cookie[] cookies = request.getCookies();
-		HashMap<String, Object> cookies_map = new HashMap<String, Object>();
-		if(cookies!=null){
-			for(int i=0; i<cookies.length; i++){
-				cookies_map.put(cookies[i].getName(), cookies[i].getValue());
-			}
-			Utils.logv(classname, "cookie: "+cookies_map);
-		}
-		
+		Utils.logv(classname, "servlet task - start");
+		long startTime = System.currentTimeMillis();
+
 		JsonObject output = null;
 		try {
 			Utils.logv(classname, "trying to extract json object from request");
@@ -58,7 +43,7 @@ public class Authentication extends HttpServlet{
 		}
 
 		PrintWriter out = response.getWriter();
-		
+
 		if (output != null) {
 			response.setContentType(Utils.JSON_TYPE);
 			response.setContentLength(output.toString().getBytes().length);
@@ -66,7 +51,7 @@ public class Authentication extends HttpServlet{
 			Utils.logv(classname, "response: "+output.toString());
 		}else {
 			output = new JsonObject();
-			output.addProperty("status",3); // error processing request
+			output.addProperty("status",-1); // error processing request
 			response.setContentType(Utils.JSON_TYPE);
 			response.setContentLength(output.toString().getBytes().length);
 			out.print(output);
@@ -75,24 +60,40 @@ public class Authentication extends HttpServlet{
 
 		out.flush();
 		out.close();
+
+		Utils.logv(classname,"servlet task - end");
+		long stopTime = System.currentTimeMillis();
+		long elapsedTime = stopTime - startTime;
+		Utils.logv(classname,elapsedTime+"ms");
 	}
 
 	private JsonObject _processInput(JsonObject input, HttpServletRequest request, HttpServletResponse response) {
+		HttpSession mySession = request.getSession(true);
+
+		Utils.logv(classname, "Protocol: "+request.getProtocol());
+		Utils.logv(classname, "Ip: "+request.getRemoteHost());
+		Utils.logv(classname, "port: "+request.getRemotePort());
+
 		JsonObject output = new JsonObject();
 		String uid = input.get("uid").getAsString();
 		String pwd = input.get("pwd").getAsString();
 		if(ClassRoom.serveronline){
 			UserProfile user = ClassRoom.users_map.get(uid);
 			if(user!=null && user.password.equals(pwd)){
-		    	output.addProperty("status",2); // authentication success
-		    	Cookie cookie = new Cookie("uid", uid);
-		    	response.addCookie(cookie);
-		    	Utils.logv(classname, "New cookie added: "+cookie);
-		    	output.addProperty("name", user.name);
-		    	output.addProperty("clsnm", ClassRoom.clsnm);
-		    }else{
-		    	output.addProperty("status", 1); // authentication failed
-		    }
+				mySession.setAttribute("username", uid);
+				mySession.setAttribute("password", pwd);
+
+				user.ipaddress = request.getRemoteHost();
+				user.SESSIONID = mySession.getId();
+				user.status = 1; // logged in
+
+				output.addProperty("status",2); // authentication success
+				output.addProperty("name", user.name);
+				output.addProperty("clsnm", ClassRoom.clsnm);
+				output.addProperty("ip", user.ipaddress);
+			}else{
+				output.addProperty("status", 1); // authentication failed
+			}
 		}else{
 			output.addProperty("status",0); //server is not ready
 		}
