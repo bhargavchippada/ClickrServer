bc880780c250e74ea8ffca59882638bffd130c5e
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-04-02 22:26:17 +0530

    ReceiveAnswer servlet done

--- src/datahandler/UserResponse.java
+++ src/datahandler/UserResponse.java
@@ -1,19 +1,33 @@
 package datahandler;
 
+import support.Utils;
+
 import com.google.gson.JsonArray;
 
 public class UserResponse {
-	public String username;
-	public String QID;
-	public Boolean correct;
+	
+	private static String classname = "UserResponse";
+	
+	public String username="NULL";
+	public String QID="NULL";
+	public Boolean correct=false;
 	public int timeTook=-1; // in secs
 	public int submitTime=-1;
 	public JsonArray answers = new JsonArray();
 
 	public String responseString(){
 		String output;
-		if(correct) output="correct\n";
-		else output="wrong\n";
+		if(correct) output="correct answer!\n";
+		else output="wrong answer!\n";
 		return output;
+	}
+	
+	public void print(){
+		Utils.logv(classname, "username: "+username);
+		Utils.logv(classname, "QID: "+QID);
+		Utils.logv(classname, "correct: "+correct);
+		Utils.logv(classname, "timeTook: "+timeTook);
+		Utils.logv(classname, "submitTime: "+submitTime);
+		Utils.logv(classname, "answers: "+answers);
 	}
 }
