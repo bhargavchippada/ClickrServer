4d2cfc0b195b09b4662a4201a04aee92e74c1364
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-04-06 03:09:30 +0530

    sending a different status code when the user response for that particular quiz is available

--- src/webconnectionjdbc/PushQuiz.java
+++ src/webconnectionjdbc/PushQuiz.java
@@ -9,6 +9,7 @@ import com.google.gson.JsonObject;
 import datahandler.ClassRoom;
 import datahandler.Question;
 import datahandler.UserProfile;
+import datahandler.UserResponse;
 
 public class PushQuiz extends JSONHttpServlet{
 	private static final long serialVersionUID = 3622456660944045304L;
@@ -32,15 +33,24 @@ public class PushQuiz extends JSONHttpServlet{
 			if(username!=null && password!=null){
 				UserProfile user = ClassRoom.users_map.get((String)username);
 				if(user!=null && user.password.equals((String) password) && uid.equals((String) username)){
-					output.addProperty("status",2); //user quiz can start
-					user.status = 2;
-					output.addProperty("title", Question.title);
-					output.addProperty("question", Question.question);
-					output.addProperty("type", Question.type);
-					output.add("options", Question.options);
-					output.addProperty("feedback", Question.feedback);
-					output.addProperty("timed", Question.timed);
-					output.addProperty("time", Question.time);
+
+					UserResponse userresp = ClassRoom.users_responsemap.get((String)username);
+					
+					if(userresp==null || (Question.savedquiz && userresp.QID!=Question.ID)){
+						output.addProperty("status",2); //user quiz can start
+						user.status = 2;
+						output.addProperty("qid", Question.ID);
+						output.addProperty("title", Question.title);
+						output.addProperty("question", Question.question);
+						output.addProperty("type", Question.type);
+						output.add("options", Question.options);
+						output.addProperty("feedback", Question.feedback);
+						output.addProperty("timed", Question.timed);
+						output.addProperty("time", Question.time);
+					}else{
+						// this user has already attempted the quiz
+						output.addProperty("status",3); //user already attempted
+					}
 				}else{
 					output.addProperty("status",0); //not authorized
 				}
