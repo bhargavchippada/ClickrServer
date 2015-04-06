38dc9dfdd792ad253e3efcb3d5efe5936f0439b3
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-04-06 10:55:21 +0530

    sending usersinfo to webinterface whenever refreshing the page

---
+++ src/webinterfacehandler/UsersInfo.java
@@ -0,0 +1,68 @@
+package webinterfacehandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import support.Utils;
+
+import com.google.gson.JsonObject;
+
+import datahandler.AdminProfile;
+import datahandler.ClassRoom;
+
+public class UsersInfo extends HttpServlet{
+
+	private static final long serialVersionUID = 8316579451970438895L;
+	String classname = "UsersInfo";
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Utils.logv(classname, "servlet task - start");
+		long startTime = System.currentTimeMillis();
+
+		JsonObject responseJson = new JsonObject();
+		response.setContentType(Utils.JSON_TYPE);
+		response.setHeader("Cache-Control", "nocache");
+
+		HttpSession mySession = request.getSession(false);
+		if(mySession == null){
+			responseJson.addProperty("status",-1); // no session
+		}else{
+			Object username = mySession.getAttribute("username");
+			Object password = mySession.getAttribute("password");
+			if(username!=null && password!=null && AdminProfile.adminAuthentication((String)username, (String)password)){
+				//so the user is authenticated
+				//fetch true means send data from server else update server information
+				boolean fetch = (boolean) Boolean.valueOf(request.getParameter("fetch"));
+				if(fetch){
+					responseJson.addProperty("status",1); // success
+					responseJson.addProperty("usersdata",ClassRoom.getClassroomJson().toString());
+					Utils.logv(classname, "fetch: true");
+				}else{
+					// no actions
+				}
+			}else{
+				responseJson.addProperty("status",-1); // authentication failure
+				Utils.logv(classname, "authentication failure");
+			}
+		}
+
+		PrintWriter out = response.getWriter();
+		out.print(responseJson.toString());
+		Utils.logv(classname, "response: "+responseJson.toString());
+		out.flush();
+		out.close();
+
+		Utils.logv(classname,"servlet task - end");
+		long stopTime = System.currentTimeMillis();
+		long elapsedTime = stopTime - startTime;
+		Utils.logv(classname,elapsedTime+"ms");
+	}
+}
