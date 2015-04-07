996ffdd925560f0ba3cf8a4c32f2722274455896
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-04-07 06:39:01 +0530

    fixed the num of matches bug

---
+++ WebContent/files/genusers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+static const char alphanum[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+int stringLength = sizeof(alphanum) - 1;
+
+char genRandom()  // Random string generator function.
+{
+
+    return alphanum[rand() % stringLength];
+}
+
+string randomStr(){
+	string Str;
+	for(unsigned int j = 0; j < 7; ++j){
+		Str += genRandom();
+	}
+	return Str;
+}
+
+int main(){
+	long base = 120050000;
+	long temp;
+	for(int i=1;i<=200;i++){
+		temp = base+i;
+		cout<<temp<<","<<temp<<","<<randomStr()<<"\n";
+	}
+}
